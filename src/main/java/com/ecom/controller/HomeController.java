bce2a9ebeb21fdcf4a3a54d8ca1449056e45a67d
debranadutta <debranadutta>
2024-11-29 23:10:07 +0530

    first commit

---
+++ src/main/java/com/ecom/controller/HomeController.java
@@ -0,0 +1,36 @@
+package com.ecom.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+public class HomeController {
+	@GetMapping("/")
+	public String index() {
+		return "Index.html";
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, path = "/login")
+	public String login() {
+		return "login";
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, path = "/register")
+	public String register() {
+		return "Registration.html";
+	}
+	
+	@GetMapping("/products")
+	public String prodcut(){
+		return "Product.html";
+	}
+	
+	@GetMapping("/product")
+	public String products() {
+		return "view_product.html";
+	}
+	
+}
